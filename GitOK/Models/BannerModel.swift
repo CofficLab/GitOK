17920e73ff4708329bcfd27f5c36f7c2a8813cb2
nookery <nooks@qq.com>
2024-05-06 11:33:13 +0800

    🎨 Auto Committed by GitOK

--- GitOK/Models/BannerModel.swift
+++ GitOK/Models/BannerModel.swift
@@ -54,7 +54,7 @@ extension BannerModel {
     func toJSONString() -> String? {
         do {
             let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
+            encoder.outputFormatting = .sortedKeys
             let jsonData = try encoder.encode(self)
             if let jsonString = String(data: jsonData, encoding: .utf8) {
                 return jsonString
