ee38ebd1e3fe5d513c9dab0c7a379099216494a2
Nookery <nooks@qq.com>
2024-09-23 17:58:31 +0800

    🎨 Chore:  Minor adjustments

--- GitOK/Terminal/Git/CommitExt.swift
+++ GitOK/Terminal/Git/CommitExt.swift
@@ -4,11 +4,11 @@ import SwiftUI
 
 extension Git {
     // MARK: 查
-    
+
     func show(_ path: String, hash: String) throws -> String {
         try run("show \(hash)", path: path)
     }
-    
+
     func commitFiles(_ path: String, hash: String) throws -> [File] {
         try run("show \(hash) --pretty='' --name-only", path: path)
             .components(separatedBy: "\n")
@@ -16,10 +16,10 @@ extension Git {
                 File.fromLine($0, path: path)
             })
     }
-    
+
     func add(_ path: String, verbose: Bool = false) throws {
         let message = try run("add -A .", path: path)
-        
+
         if verbose {
             os_log("\(self.label)Add -> \(message)")
         }
@@ -28,38 +28,38 @@ extension Git {
     func commit(_ path: String, commit: String) throws -> String {
         try run("commit -a -m '\(commit)'", path: path)
     }
-    
+
     func commitAndPush(_ message: String, path: String) throws {
-        DispatchQueue.main.async {
-            do {
-                // 检查是否通过HTTPS进行push
-                let shell = Shell()
-                let remoteUrl = try shell.run("git config --get remote.origin.url", at: path)
-                if remoteUrl.starts(with: "https://") {
-                    // Git凭据缓存
-                    let cacheResult = shell.configureGitCredentialCache()
-                    os_log("\(shell.label)Configure Git Credential Cache -> \(cacheResult)")
-                    
-                    // 检查HTTPS凭据
-                    let commit = GitCommit.headFor(path)
-                    guard commit.checkHttpsCredentials() else {
-                        os_log(.error, "HTTPS 凭据未配置")
-                        throw GitError.credentialsNotConfigured
-                    }
-                }
+        do {
+            // 检查是否通过HTTPS进行push
+            let shell = Shell()
+            let remoteUrl = try shell.run("git config --get remote.origin.url", at: path)
+            if remoteUrl.starts(with: "https://") {
+                // Git凭据缓存
+                let cacheResult = shell.configureGitCredentialCache()
+                os_log("\(shell.label)Configure Git Credential Cache -> \(cacheResult)")
 
-                do {
-                    try shell.run("git add .", at: path)
-                    try shell.run("git commit -m \"\(message)\"", at: path)
-                } catch let error {
-                    os_log(.error, "提交失败: \(error.localizedDescription)")
-                    throw error
+                // 检查HTTPS凭据
+                let commit = GitCommit.headFor(path)
+                guard commit.checkHttpsCredentials() else {
+                    os_log(.error, "HTTPS 凭据未配置")
+                    throw GitError.credentialsNotConfigured
                 }
-
-                try self.push(path)
-            } catch let error {
-                os_log(.error, "commitAndPush 失败: \(error.localizedDescription)")
             }
+
+            do {
+                try shell.run("git add .", at: path)
+                try shell.run("git commit -m \"\(message)\"", at: path)
+            } catch let error {
+                os_log(.error, "提交失败: \(error.localizedDescription)")
+                throw error
+            }
+
+            try self.push(path)
+        } catch let error {
+            os_log(.error, "\(self.label)commitAndPush 失败: \(error.localizedDescription)")
+
+            throw error
         }
     }
 
