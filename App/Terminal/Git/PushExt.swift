ed83cd40f3717d5db4702c38526e8c56fc123c28
Nookery <nooks@qq.com>
2024-09-23 20:38:11 +0800

    🎨 Chore:  Minor adjustments

--- App/Terminal/Git/PushExt.swift
+++ App/Terminal/Git/PushExt.swift
@@ -6,9 +6,12 @@ extension Git {
     func push(_ path: String) throws {
         let shell = Shell()
         do {
+            self.emitGitPushing()
             _ = try shell.run("git push", at: path)
+            self.emitGitPushSuccess()
         } catch let error {
             os_log(.error, "推送失败: \(error.localizedDescription)")
+            self.emitGitPushFailed()
             throw error
         }
     }
