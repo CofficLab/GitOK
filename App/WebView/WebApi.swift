0c87ff0eb8f12d3d68770a6611449a6e762e4ede
Nookery <nooks@qq.com>
2024-10-14 15:52:15 +0800

    🐛 Bugfix:  Fix a bug

--- App/WebView/WebApi.swift
+++ App/WebView/WebApi.swift
@@ -7,22 +7,30 @@ extension WebContent {
         encoder.outputFormatting = .prettyPrinted
         let jsonData = try! encoder.encode([
             "original": o,
-            "modified": c
+            "modified": c,
         ])
         let jsonString = String(data: jsonData, encoding: .utf8)!
 
-        run("window.api.setTextsWithObject(\(jsonString))")
+        Task {
+            try? await run("window.api.setTextsWithObject(\(jsonString))")
+        }
     }
-    
+
     @objc func setOriginal(_ s: String) {
-        run("window.api.setOriginal(`\(s)`)")
+        Task {
+            try? await run("window.api.setOriginal(`\(s)`)")
+        }
     }
-    
+
     @objc func setModified(_ s: String) {
-        run("window.api.setModified(`\(s)`)")
+        Task {
+            try? await run("window.api.setModified(`\(s)`)")
+        }
     }
-    
+
     @objc func getOriginal() {
-        run("window.api.original")
+        Task {
+            try? await run("window.api.original")
+        }
     }
 }
