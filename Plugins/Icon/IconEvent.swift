71186673401cc65496240ffba80b33f47e43832a
Nookery <nooks@qq.com>
2024-12-10 23:42:57 +0800

    🗑️ Trash:  Delete unnecessary code or files

--- Plugins/Icon/IconEvent.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Notification.Name {
-    static let iconDidChange = Notification.Name("iconDidChange")
-    static let iconDidSave = Notification.Name("iconDidSave")
-    static let iconDidFail = Notification.Name("iconDidFail")
-    static let iconDidGet = Notification.Name("iconDidGet")
-    static let iconTitleDidChange = Notification.Name("iconTitleDidChange")
-}
