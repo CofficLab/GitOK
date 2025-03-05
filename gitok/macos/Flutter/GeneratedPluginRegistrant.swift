3d809301496b9715f9965b0718bbfbef70a69e13
Nookery <nooks@qq.com>
2025-03-05 18:40:43 +0800

    🚀 perf: improve loading performance

--- gitok/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gitok/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import hotkey_manager_macos
 import macos_window_utils
 import screen_retriever_macos
 import shared_preferences_foundation
+import tray_manager
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
