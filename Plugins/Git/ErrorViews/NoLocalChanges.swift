69f467960425b529ffec4ed4f9e816fa7c05b0df
Nookery <nooks@qq.com>
2025-06-07 12:38:52 +0800

    🎨 Chore:  Minor adjustments

--- Plugins/Git/ErrorViews/NoLocalChanges.swift
+++ Plugins/Git/ErrorViews/NoLocalChanges.swift
@@ -8,7 +8,7 @@ struct NoLocalChanges: View {
             systemImage: "checkmark.circle",
             title: "没有本地更改",
             subtitle: "全部更改已提交到本地仓库"
-        )
+        ).setIconColor(.green)
     }
 }
 
