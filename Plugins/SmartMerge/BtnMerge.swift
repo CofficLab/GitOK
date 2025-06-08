72bdf0b925cf446eef23c7d480cd3c25da600009
Nookery <nooks@qq.com>
2025-06-08 22:05:57 +0800

    🎨 Chore:  Minor adjustments

--- Plugins/SmartMerge/BtnMerge.swift
+++ Plugins/SmartMerge/BtnMerge.swift
@@ -24,15 +24,13 @@ struct BtnMerge: View, SuperEvent, SuperThread {
     }
 
     func merge() {
-        self.bg.async {
-            do {
-                try GitShell.setBranch(to, path)
-                try GitShell.merge(from, path, message: CommitCategory.CI.text + "Merge \(from.name) by GitOK")
-            } catch let error {
-                os_log(.error, "\(error.localizedDescription)")
+        do {
+            try GitShell.setBranch(to, path)
+            try GitShell.merge(from, path, message: CommitCategory.CI.text + "Merge \(from.name) by GitOK")
+        } catch let error {
+            os_log(.error, "\(error.localizedDescription)")
 
-                m.setError(error)
-            }
+            m.setError(error)
         }
     }
 }
