3bafe4af7b83fc1e50631f3e3082fb6684204254
nookery <nooks@foxmail.com>
2025-06-10 14:55:04 +0800

    🐛 Bugfix:  Fix a bug

--- Plugins/OpenCursor/BtnOpenCursorView.swift
+++ Plugins/OpenCursor/BtnOpenCursorView.swift
@@ -2,29 +2,43 @@ import SwiftUI
 
 struct BtnOpenCursorView: View {
     @EnvironmentObject var g: DataProvider
-    
+
     static let shared = BtnOpenCursorView()
-    
+
     private init() {}
 
     var body: some View {
         if let project = g.project {
             Button(action: {
-                guard
-                    let appUrl = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.cursor.Cursor")
-                else { return }
-
-                NSWorkspace.shared.open([project.url], withApplicationAt: appUrl, configuration: NSWorkspace.OpenConfiguration())
+                if let vscodeURL = NSWorkspace.shared.urlForApplication(toOpen: URL(fileURLWithPath: "/Applications/Cursor.app")) {
+                    NSWorkspace.shared.open([project.url], withApplicationAt: vscodeURL, configuration: NSWorkspace.OpenConfiguration(), completionHandler: nil)
+                }
             }, label: {
                 Label(
-                    title: { Text("用Cursor打开") },
+                    title: { Text("用 Cursor 打开") },
                     icon: {
-                        Image("Cursor")
-                            .resizable()
-                            .frame(width: 24, height: 24)
+                        Image("Cursor").resizable().scaledToFit().scaleEffect(0.9)
                     }
                 )
             })
         }
     }
+}
+
+#Preview("App - Small Screen") {
+    RootView {
+        ContentLayout()
+            .hideSidebar()
+            .hideProjectActions()
+    }
+    .frame(width: 600)
+    .frame(height: 600)
+}
+
+#Preview("App-Big Screen") {
+    RootView {
+        ContentLayout()
+    }
+    .frame(width: 1200)
+    .frame(height: 1200)
 }
