83a712f7c86de770e9fdf2ee30b9b247756304be
Nookery <nooks@qq.com>
2025-06-14 22:01:47 +0800

    🎨 Chore:  Minor adjustments

--- Plugins/Git-Commit/CommitForm.swift
+++ Plugins/Git-Commit/CommitForm.swift
@@ -1,6 +1,6 @@
-import SwiftUI
-import OSLog
 import MagicCore
+import OSLog
+import SwiftUI
 
 struct CommitForm: View, SuperLog {
     @EnvironmentObject var app: AppProvider
@@ -19,57 +19,67 @@ struct CommitForm: View, SuperLog {
     }
 
     var body: some View {
-        if let project = g.project {
-            VStack(spacing: 0) {
-                HStack(spacing: 0) {
-                    // 当前分支信息显示区域
-                    if let currentBranch = g.branch {
-                        HStack {
-                            Image(systemName: "arrow.branch")
-                                .foregroundColor(.secondary)
-                                .font(.system(size: 14))
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                // 当前分支信息显示区域
+                if let currentBranch = g.branch {
+                    HStack {
+                        Image(systemName: "arrow.branch")
+                            .foregroundColor(.secondary)
+                            .font(.system(size: 14))
 
-                            Text(currentBranch.name)
-                                .font(.caption)
-                                .fontWeight(.medium)
-                                .foregroundColor(.primary)
-                        }
-                        .padding(.trailing, 12)
+                        Text(currentBranch.name)
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.primary)
                     }
+                    .padding(.trailing, 12)
+                }
 
-                    Picker("", selection: $category, content: {
-                        ForEach(CommitCategory.allCases, id: \.self, content: {
-                            Text($0.label).tag($0 as CommitCategory?)
-                        })
+                Picker("", selection: $category, content: {
+                    ForEach(CommitCategory.allCases, id: \.self, content: {
+                        Text($0.label).tag($0 as CommitCategory?)
                     })
-                    .frame(width: 135)
-                    .pickerStyle(.automatic)
+                })
+                .frame(width: 135)
+                .pickerStyle(.automatic)
 
-                    Spacer()
-                    TextField("commit", text: $text)
-                        .textFieldStyle(.roundedBorder)
-                        .padding(.vertical)
-                }
-
-                HStack {
-                    UserView()
-
-                    BtnCommitAndPush(commitMessage: commitMessage)
-                }
-                .frame(height: 40)
+                Spacer()
+                TextField("commit", text: $text)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
             }
-            .onNotification(.projectDidCommit, perform: { _ in
-                self.text = self.category.defaultMessage
-            })
-            .onChange(of: category, {
-                self.text = category.defaultMessage
-            })
-            .onAppear {
-                self.text = self.category.defaultMessage
+
+            HStack {
+                UserView()
+
+                BtnCommitAndPush(commitMessage: commitMessage)
             }
+            .frame(height: 40)
         }
+        .onNotification(.projectDidCommit, perform: onProjectDidCommit)
+        .onChange(of: category, onCategoryDidChange)
+        .onAppear(perform: onAppear)
     }
 }
+
+// MARK: - Action
+
+extension CommitForm {
+    func onProjectDidCommit(_ notification: Notification) {
+        self.text = self.category.defaultMessage
+    }
+
+    func onCategoryDidChange() {
+        self.text = self.category.defaultMessage
+    }
+
+    func onAppear() {
+        self.text = self.category.defaultMessage
+    }
+}
+
+// MARK: - Preview
 
 #Preview("App - Small Screen") {
     RootView {
