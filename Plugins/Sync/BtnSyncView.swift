1320e7e392f380455d6cec169a072676c4d2a4a5
Nookery <nooks@qq.com>
2025-06-15 21:11:05 +0800

    🎨 Chore:  Minor adjustments

--- Plugins/Sync/BtnSyncView.swift
+++ Plugins/Sync/BtnSyncView.swift
@@ -10,44 +10,51 @@ struct BtnSyncView: View, SuperLog, SuperEvent, SuperThread {
     @State var isGitProject = false
 
     var commitMessage = CommitCategory.auto
-    
+
     static let shared = BtnSyncView()
-    
+
     private init() {}
 
     var body: some View {
-        if let project = data.project, self.isGitProject {
-            Button(action: {
-                sync(path: project.path)
-            }, label: {
-                Label("同步", systemImage: "arrow.triangle.2.circlepath")
-                    .rotationEffect(Angle(degrees: self.rotationAngle))
-            })
-            .help("和远程仓库同步")
-            .disabled(working)
-            .onAppear(perform: onAppear)
-            .onChange(of: working) {
-                let duration = 0.02
-                if working {
-                    Timer.scheduledTimer(withTimeInterval: duration, repeats: true) { timer in
-                        if !working {
-                            timer.invalidate()
-                            withAnimation(.easeInOut(duration: duration)) {
-                                rotationAngle = 0.0
-                            }
-                        } else {
-                            withAnimation(.easeInOut(duration: duration)) {
-                                rotationAngle += 7
+        ZStack {
+            if let project = data.project, self.isGitProject {
+                MagicButton(icon: .iconSync) { completion in
+                    sync(path: project.path)
+                    completion()
+                }
+                .magicShape(.circle)
+                .magicStyle(.secondary)
+                .magicShapeVisibility(.onHover)
+                .help("和远程仓库同步")
+                .disabled(working)
+                .onAppear(perform: onAppear)
+                .onChange(of: working) {
+                    let duration = 0.02
+                    if working {
+                        Timer.scheduledTimer(withTimeInterval: duration, repeats: true) { timer in
+                            if !working {
+                                timer.invalidate()
+                                withAnimation(.easeInOut(duration: duration)) {
+                                    rotationAngle = 0.0
+                                }
+                            } else {
+                                withAnimation(.easeInOut(duration: duration)) {
+                                    rotationAngle += 7
+                                }
                             }
                         }
-                    }
-                } else {
-                    withAnimation(.easeInOut(duration: 0.1)) {
-                        rotationAngle = 0.0
+                    } else {
+                        withAnimation(.easeInOut(duration: 0.1)) {
+                            rotationAngle = 0.0
+                        }
                     }
                 }
+            } else {
+                // 空状态占位符，确保视图始终有内容
+                Color.clear.frame(width: 24, height: 24)
             }
         }
+        .onAppear(perform: onAppear)
     }
 
     func sync(path: String) {
@@ -60,7 +67,7 @@ struct BtnSyncView: View, SuperLog, SuperEvent, SuperThread {
 
         do {
             try self.data.project?.sync()
-            
+
             // 隐藏加载状态 - 成功消息会通过Project的事件系统自动显示
             m.hideLoading()
             self.reset()
