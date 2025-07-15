fe32d3c0cab613fdb783d9c899a5ce27ec16ff00
nooks <nooks@qq.com>
2025-07-15 13:31:39 +0800

    💄 UI: Update icon assets and scaling implementation

--- Plugins/Icon/ToolBar/IconScale.swift
+++ Plugins/Icon/ToolBar/IconScale.swift
@@ -7,29 +7,31 @@ struct IconScale: View {
     @EnvironmentObject var app: AppProvider
     
     @State var icon: IconModel?
+    @State var scale: Double = 1
     
     var body: some View {
         VStack {
             if icon != nil {
-                Slider(value: Binding(
-                    get: { self.icon?.scale ?? 1.0 },
-                    set: { newScale in
-                        if self.icon != nil {
-                            do {
-                                try self.icon!.updateScale(newScale)
-                            } catch {
-                                m.error(error.localizedDescription)
-                            }
-                        }
-                    }
-                ), in: 0.1 ... 2)
+                Slider(value: $scale, in: 0.1 ... 2)
                     .padding()
-            } else {
-                Text("没有可用的图标")
             }
         }
         .onAppear {
             self.icon = try? i.getIcon()
+            self.scale = self.icon?.scale ?? 1.0
+        }
+        .onChange(of: scale) {
+            if var icon = try? self.i.getIcon() {
+                do {
+                    try icon.updateScale(scale)
+                } catch {
+                    m.error(error.localizedDescription)
+                }
+            }
+        }
+        .onChange(of: self.i.iconURL) {
+            self.icon = try? i.getIcon()
+            self.scale = self.icon?.scale ?? 1.0
         }
         .padding()
     }
