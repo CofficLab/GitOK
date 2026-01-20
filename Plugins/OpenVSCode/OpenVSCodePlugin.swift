9470f42ca321434a62939924ed1d10dd27e1dad5
Nookery <nooks@qq.com>
2026-01-20 20:36:04 +0800

    🎨 Chore: Use icon constant instead of string literal in OpenVSCodePlugin

--- Plugins/OpenVSCode/OpenVSCodePlugin.swift
+++ Plugins/OpenVSCode/OpenVSCodePlugin.swift
@@ -26,7 +26,7 @@ class OpenVSCodePlugin: SuperPlugin, SuperLog, PluginRegistrant {
     static var description: String = "在 VS Code 中打开当前项目"
 
     /// 插件图标名称
-    static var iconName: String = "code"
+    static var iconName: String = .iconCode
 
     /// 插件是否可配置（是否在设置中由用户控制启用/停用）
     static var isConfigurable: Bool = true
