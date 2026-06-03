01c0640c350da11156c1cfea83df02412ccde4a5
nookery <nooks@qq.com>
2026-06-03 21:21:44 +0800

    refactor(ThemeSpringPlugin): use package localizations

--- Plugins/ThemeSpringPlugin/Sources/SpringThemePlugin.swift
+++ Plugins/ThemeSpringPlugin/Sources/SpringThemePlugin.swift
@@ -34,6 +34,6 @@ public enum SpringThemePluginLocalization {
     public static let bundle = Bundle.module
 
     public static func string(_ key: String) -> String {
-        NSLocalizedString(key, tableName: table, bundle: bundle, value: key, comment: "")
+        String(localized: String.LocalizationValue(key), bundle: .module, comment: "")
     }
 }
