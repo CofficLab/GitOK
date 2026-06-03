cc1324b866a08018fee04209736ea18dda866264
nookery <nooks@qq.com>
2026-06-03 21:23:01 +0800

    refactor(UnpushedStatusPlugin): use package localizations

--- Plugins/UnpushedStatusPlugin/Sources/UnpushedStatusPlugin.swift
+++ Plugins/UnpushedStatusPlugin/Sources/UnpushedStatusPlugin.swift
@@ -33,6 +33,6 @@ public enum UnpushedStatusPluginLocalization {
     public static let bundle = Bundle.module
 
     public static func string(_ key: String) -> String {
-        NSLocalizedString(key, tableName: table, bundle: bundle, value: key, comment: "")
+        String(localized: String.LocalizationValue(key), bundle: .module, comment: "")
     }
 }
