4e0a123cc21012772da30ba01e7160e98ee6b5e4
nookery <nooks@qq.com>
2026-06-03 21:19:02 +0800

    refactor(ThemeGlacierPlugin): use package localizations

--- Plugins/ThemeGlacierPlugin/Sources/GlacierThemePlugin.swift
+++ Plugins/ThemeGlacierPlugin/Sources/GlacierThemePlugin.swift
@@ -18,6 +18,6 @@ public enum GlacierThemePluginLocalization {
     public static let bundle = Bundle.module
 
     public static func string(_ key: String) -> String {
-        NSLocalizedString(key, tableName: table, bundle: bundle, value: key, comment: "")
+        String(localized: String.LocalizationValue(key), bundle: .module, comment: "")
     }
 }
