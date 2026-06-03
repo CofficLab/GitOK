904c8d4644bdf3055217e4d39f0c7ca15bb64487
nookery <nooks@qq.com>
2026-06-03 21:22:07 +0800

    refactor(ThemeSummerPlugin): use package localizations

--- Plugins/ThemeSummerPlugin/Sources/SummerThemePlugin.swift
+++ Plugins/ThemeSummerPlugin/Sources/SummerThemePlugin.swift
@@ -35,6 +35,6 @@ public enum SummerThemePluginLocalization {
     public static let bundle = Bundle.module
 
     public static func string(_ key: String) -> String {
-        NSLocalizedString(key, tableName: table, bundle: bundle, value: key, comment: "")
+        String(localized: String.LocalizationValue(key), bundle: .module, comment: "")
     }
 }
