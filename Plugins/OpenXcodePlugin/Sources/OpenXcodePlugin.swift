8ef387daf9a4d52b6bf3b19cc41987d5a99547b2
nookery <nooks@qq.com>
2026-06-03 21:16:33 +0800

    refactor(OpenXcodePlugin): use package localizations

--- Plugins/OpenXcodePlugin/Sources/OpenXcodePlugin.swift
+++ Plugins/OpenXcodePlugin/Sources/OpenXcodePlugin.swift
@@ -28,6 +28,6 @@ public enum OpenXcodePluginLocalization {
     public static let bundle = Bundle.module
 
     public static func string(_ key: String) -> String {
-        NSLocalizedString(key, tableName: table, bundle: bundle, value: key, comment: "")
+        String(localized: String.LocalizationValue(key), bundle: .module, comment: "")
     }
 }
