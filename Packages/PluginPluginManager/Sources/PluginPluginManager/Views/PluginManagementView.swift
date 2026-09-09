b6456e65e0f4525b46751d414f8277abb1047618
nookery <nooks@qq.com>
2026-09-09 09:44:47 +0800

    fix: refine settings theme previews

--- Packages/PluginPluginManager/Sources/PluginPluginManager/Views/PluginManagementView.swift
+++ Packages/PluginPluginManager/Sources/PluginPluginManager/Views/PluginManagementView.swift
@@ -60,6 +60,7 @@ struct PluginManagementView: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
+        .padding(.bottom, 16)
         .onAppear {
             if selectedPluginID == nil {
                 selectedPluginID = selectedPlugin?.id
